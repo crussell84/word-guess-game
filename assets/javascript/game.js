5b03127a87e29e0980e182e0e179d4bf33f5e5e2
Chris Russell <christine.russell10@gmail.com>
2018-06-25 17:09:29 -0400

    notes in JS file for potential options

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,3 +1,9 @@
 var wordBank =["dragon", "princess", "knight", "castle", "sword", "lance", "tournament", "wizard", "magic", "monster"];
 var selectedWord = wordBank[[Math.floor(Math.random() * wordBank.length)]]
 console.log(selectedWord);
+
+//need to keep track of and display letters guessed, number of guesses remaining, number of wins
+//compare guessed letter to letters in word & display matches in correct spot
+///display a series of _ to mark number of letters (underlines? or borders? way to make it a 'reveal' of letters vid hide/show?)
+//remove used words from bank?
+//use modals instead of alerts?
