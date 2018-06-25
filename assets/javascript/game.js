e0058762aeb2caf1c54c411cacc998700a5b9f88
Chris Russell <christine.russell10@gmail.com>
2018-06-25 17:02:07 -0400

    HTML skeleton, select random word from bank

---
+++ assets/javascript/game.js
@@ -0,0 +1,3 @@
+var wordBank =["dragon", "princess", "knight", "castle", "sword", "lance", "tournament", "wizard", "magic", "monster"];
+var selectedWord = wordBank[[Math.floor(Math.random() * wordBank.length)]]
+console.log(selectedWord);
