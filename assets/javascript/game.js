1740a8d894f0dd4a3647157e689a6e92ae6306e3
Chris Russell <christine.russell10@gmail.com>
2018-06-25 18:27:31 -0400

    start of game logic to capture key press & check if in word

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,9 +1,31 @@
 var wordBank =["dragon", "princess", "knight", "castle", "sword", "lance", "tournament", "wizard", "magic", "monster"];
 var selectedWord = wordBank[[Math.floor(Math.random() * wordBank.length)]]
 console.log(selectedWord);
+var guessesRemaining = 12;
 
 //need to keep track of and display letters guessed, number of guesses remaining, number of wins
 //compare guessed letter to letters in word & display matches in correct spot
 ///display a series of _ to mark number of letters (underlines? or borders? way to make it a 'reveal' of letters vid hide/show?)
 //remove used words from bank?
 //use modals instead of alerts?
+
+
+document.onkeyup = function (event){
+ var userGuess = event.key;
+
+ userGuess = userGuess.toLowerCase();
+ //add logic to check if keypressed is letter
+ //add logic to check if letter already guessed
+ //add logic for when guessesRemaining hits 0
+if (selectedWord.includes(userGuess)) {
+    //update display of selected word & guessed letters
+    console.log(userGuess);
+    guessesRemaining--;
+}
+else {
+    //update display of guessed letters & guesses remaining
+    guessesRemaining--;
+}
+
+
+}
