b964bdd49f8c7471db2d5f1dc7cdaec35a94c3f8
Chris Russell <christine.russell10@gmail.com>
2018-06-25 23:07:31 -0400

    added logic to validate userguess & update display word

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,31 +1,56 @@
-var wordBank =["dragon", "princess", "knight", "castle", "sword", "lance", "tournament", "wizard", "magic", "monster"];
+var wordBank = ["dragon", "princess", "knight", "castle", "sword", "lance", "tournament", "wizard", "magic", "monster"];
 var selectedWord = wordBank[[Math.floor(Math.random() * wordBank.length)]]
 console.log(selectedWord);
 var guessesRemaining = 12;
+var guessedLetters = [];
+var displayedWord = "";
 
 //need to keep track of and display letters guessed, number of guesses remaining, number of wins
 //compare guessed letter to letters in word & display matches in correct spot
 ///display a series of _ to mark number of letters (underlines? or borders? way to make it a 'reveal' of letters vid hide/show?)
 //remove used words from bank?
 //use modals instead of alerts?
+var wordDiv = document.getElementById("wordDisplay");
 
+for (var i = 0; i < selectedWord.length; i++) {
+    displayedWord = displayedWord + "_";
+    wordDiv.textContent = displayedWord;
 
-document.onkeyup = function (event){
- var userGuess = event.key;
+}
 
- userGuess = userGuess.toLowerCase();
- //add logic to check if keypressed is letter
- //add logic to check if letter already guessed
- //add logic for when guessesRemaining hits 0
-if (selectedWord.includes(userGuess)) {
-    //update display of selected word & guessed letters
-    console.log(userGuess);
-    guessesRemaining--;
+//fucnction to replace the underscore in the display string
+function setCharAt(str,index,chr) {
+	if(index > str.length-1) return str;
+	return str.substr(0,index) + chr + str.substr(index+1);
+}
+
+document.onkeyup = function (event) {
+    var userGuess = event.key;
+
+    userGuess = userGuess.toLowerCase();
+if (userGuess.match(/[a-z]/i)) {
+    //add logic to check if keypressed is letter
+    //add logic to check if letter already guessed
+    //add logic for when guessesRemaining hits 0
+    if (selectedWord.includes(userGuess)) {
+        for (var i = 0; i < selectedWord.length; i++) {
+            if (userGuess === selectedWord.charAt(i)) {
+                displayedWord = setCharAt(displayedWord, i, userGuess);
+                wordDiv.textContent = displayedWord;
+            }
+        }
+        guessesRemaining--;
+        guessedLetters.push(userGuess);
+    }
+    else {
+        //update display of guessed letters & guesses remaining
+        guessesRemaining--;
+        guessedLetters.push(userGuess);
+    }
+
+    console.log(guessedLetters);
 }
 else {
-    //update display of guessed letters & guesses remaining
-    guessesRemaining--;
+    console.log("Not a valid guess!")
 }
-
-
 }
